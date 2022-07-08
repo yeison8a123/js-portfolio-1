4f215db859a2169c760ec4e365cf3f41a8836670
Yeison Ochoa <oyema229@gmail.com>
2022-07-08 02:12:05 -0300

    Deploy a Netlify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import Template from './templates/Template.js';
-console.log('hola');
-console.log('como estas');
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/vars.styl';
 
 (async function App() {
   const main = null || document.getElementById('main');
