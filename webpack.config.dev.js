64924c1dd83cd19d00bbcf92f44c817e0b4b62d4
Yeison Ochoa <oyema229@gmail.com>
2022-07-12 00:56:26 -0300

    actualizacion 12 de julio

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,7 +14,6 @@ module.exports = {
         assetModuleFilename: 'assetes/images/[hash][ext][query]',
     },
     mode: 'development',
-    watch: true,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -62,7 +61,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            inject: true,
+            inject: 'body',
             template: './public/index.html',
             filename: './index.html'
         }),
@@ -79,4 +78,15 @@ module.exports = {
         }),
         new Dotenv(),
     ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+            watch: true
+        },
+        watchFiles: path.join(__dirname, "./**"), 
+        compress: true,
+        historyApiFallback: true,
+        port: 3005,
+        open: true, 
+    },
 }
